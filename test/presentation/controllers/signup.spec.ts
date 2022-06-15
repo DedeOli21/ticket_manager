09c2f0ce2e61c6cefcfa5ccf87df2b42b07c260b
David Oliveira <david.willian@aluno.unincor.edu.br>
2022-06-14 21:21:14 -0300

    test: Ensure return serverError and statusCode 500

--- test/presentation/controllers/signup.spec.ts
+++ test/presentation/controllers/signup.spec.ts
@@ -1,5 +1,5 @@
 import { SignUpController } from '@/presentation/controllers/signup';
-import { MissingParamError, InvalidParamError } from '@/presentation/errors'
+import { MissingParamError, InvalidParamError, ServerError } from '@/presentation/errors'
 import { EmailValidator } from '@/presentation/protocols/email-validator'
 
 interface SutTypes {
@@ -122,5 +122,23 @@ describe('SignUp Controller', () => {
     }
     sut.handle(httpRequest)
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
+  test('Should return 500 if EmailValidator throws', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
