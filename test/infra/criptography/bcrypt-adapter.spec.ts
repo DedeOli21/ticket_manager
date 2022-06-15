5d3c7c90435a7a0069f2c6e72931529ab4abe2ae
David Oliveira <david.willian@aluno.unincor.edu.br>
2022-06-15 17:29:56 -0300

    test: add bcrypt-adapter and mock

--- test/infra/criptography/bcrypt-adapter.spec.ts
+++ test/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,9 +1,10 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter'
+import { throwError } from '@test/domain/mocks'
 
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
-    return new Promise(resolve => resolve('hash'))
+    return 'hash'
   }
 }))
 
@@ -16,13 +17,20 @@ describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    await sut.encrypt('any_value')
+    await sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   test('Should return a hash on success', async () => {
     const sut = makeSut()
-    const hash = await sut.encrypt('any_value')
+    const hash = await sut.hash('any_value')
     expect(hash).toBe('hash')
+  })
+
+  test('Should throw if hash throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(throwError)
+    const promise = sut.hash('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
